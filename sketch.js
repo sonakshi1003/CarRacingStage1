ced3549f3a71494b24c74b8cf3956b72661f860c
sonakshi1003 <sujata3dutta@gmail.com>
2020-04-28 16:57:00 +0530

    player

--- sketch.js
+++ sketch.js
@@ -5,12 +5,13 @@ var playerCount;
 var allPlayers;
 var distance = 0;
 var database;
+var cars,car1,car2,car3,car4;
 
 var form, player, game;
 
 
 function setup(){
-  canvas = createCanvas(400,400);
+  canvas = createCanvas(displayWidth - 20, displayHeight - 30);
   database = firebase.database();
   game = new Game();
   game.getState();
